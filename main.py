d632dea2748b174e6eb401891d211622c7cec6d6
Bellagrey <anapoliwilson@my365.bellevue.edu>
2020-10-08 02:07:43 +0000

    Final Project

--- main.py
+++ main.py
@@ -1,30 +1,9 @@
 '''
-
 Name: Andrea Napoli-Wilson
 Class: CIS 250
 Date: 9/25/2020
-Project name: Task 2--Draft Program in a .py File
-
-INSTRUCTIONS:
-X = "done"
-X Create a Python Application which asks the user for their zip code or city.
-X Use the zip code or city name in order to obtain weather forecast data from: http://openweathermap.org/
-X Display the weather forecast in a readable format to the user.
-X Use comments within the application where appropriate in order to document what the program is doing.
-X Use functions including a main function.
-X Allow the user to run the program multiple times.
-X Validate whether the user entered valid data. If valid data isn’t presented notify the user.
-X Use the Requests library in order to request data from the webservice.
-X Use Python 3.
-X Use try blocks when establishing connections to the webservice. You must print a message to the user indicating whether or not the connection was successful.
-Deliverables (More detail on these deliverables is provided in the the weeks they are due):
+Project name: Final Project
 '''
-
-# Python program to find current
-# weather details of any city
-# using openweathermap api
-# import required modules
-
 
 import requests
 
@@ -35,35 +14,20 @@ def fer(k):
 
 
 def get_data(url, zip=None, city=None):
-    # base_url variable to store url
-    #open_weather = "http://api.openweathermap.org/data/2.5/weather"
-    #apiid = "cba7fee375a53b5fde82e6dda8ce69a1"
-# check if user gave you the zip or the city
 
   if zip is not None:
-    #us at the end for USA
-      url += "&zip="+str(zip)+",us"
+    url += "&zip="+str(zip)+",us"
   else:
-      #url += "&q="+str(city)+",us"
-      url += "&q="+city
-      #url += "&apiid="+str(apiid)
-  #make get requests using request module
+    url += "&q="+city
+    
   r = requests.get(url)
 
-  #return response
   return r
 
 def itworks(resp):
-    #print(resp)
     if resp.status_code == 200:
         data = resp.json()
-        #print(data['name'])
-        #print("""{data['name']} Weather Forecast:
-        #There will be {data['weather'][0]['description']} with wind speed of {data['wind']['speedf']}.
-        #Visibility will be {data['visibility']}.
-        #Min. Temp will be {data{['main']['temp_min']} and max will be {data['main']['temp_nax']}.
-        #""")
-        #print(data)
+        
         my_str="""
 {name} Weather Forecast:
 There will be {description} with wind speed of {windspeed}.
@@ -75,14 +39,10 @@ Min. Temp will be {temp_min:.2f} and max will be {temp_max:.2f}.""".format(name=
 
 def main():
 
-    #Enter your API key here
     apiid = "cba7fee375a53b5fde82e6dda8ce69a1"
-
-    # base_url variable to store url
     apiurl = "http://api.openweathermap.org/data/2.5/weather?appid=" + apiid
 
     while (True):
-    #show choices
         inp =int(input("Please select one :\n1. Zip Code\n2. City Name\n3. Exit\n"))
 
         if inp == 1:
@@ -92,8 +52,7 @@ def main():
                 print("Please enter a 5-digit zip code")
                 continue
             try:
-                # See if it can be converted to an integer.
-                # Don't use the converted value, but error if it can't be converted.
+                
                 test = int(dataZip)
                 resp= get_data(apiurl,dataZip,None)
                 itworks(resp)
